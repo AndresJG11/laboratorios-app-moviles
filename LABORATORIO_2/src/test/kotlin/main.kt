8aa90cae787a77995430b310e1ce8c2b82ffe221
AndresJG <anjimenez11@hotmail.com>
2020-02-24 23:37:24 -0500

    Segundo laboratorio

--- LABORATORIO_2/src/test/kotlin/main.kt
+++ LABORATORIO_2/src/test/kotlin/main.kt
@@ -1,3 +1,22 @@
-fun main(){
-    print("hola")
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+
+
+fun httpPostAsync(url: String, data: String): Deferred<String>{
+    return GlobalScope.async{
+        httpPost(url, data)
+    }
+}
+
+suspend fun main(){
+    var url: String = "https://postman-echo.com/post"
+    var data: String = "/hi/there?hand=wave"
+    var response:String = httpPost(url, data)
+    println(response)
+
+    println("--------------------------------")
+    data = "nombre=andres&apellido=jim"
+    var asincResponse = httpPostAsync(url, data)
+    println(asincResponse.await())
 }
---
+++ LABORATORIO_2/src/test/kotlin/testHttpGet.kt
@@ -0,0 +1,13 @@
+import kotlin.concurrent.thread
+
+
+fun main(){
+
+    httpGet("http://postman-echo.com/get?foo1=bar1&foo2=bar2"){response ->
+
+        println(response)
+
+    }
+
+    Thread.sleep(1000)
+}
